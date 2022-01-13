39ddcdf2144a74d3c02c02e3edc972ee1ddf0e5c
spartacus04 <andrea.bonari.pingas@gmail.com>
2022-01-13 12:09:04 +0100

    aggiungo progetto VS2010

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EsercizioRipasso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
